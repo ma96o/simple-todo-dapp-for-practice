07f0a9edcc9e47197090d3011b112f8e46f51cf0
masaaki1915 <masaaki1915@gmail.com>
2022-06-14 18:45:41 +0900

    add functions in view for connecting contract

--- webfront/src/App.tsx
+++ webfront/src/App.tsx
@@ -1,3 +1,13 @@
 import React, { VFC } from "react";
+import { ethers } from "ethers";
+import artifact from "./abi/TodoList.json";
 
-export const App: VFC = () => <h1>Hello, TodoList Contract.</h1>
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const App: VFC = () => {
+	const provider = new ethers.providers.JsonRpcProvider();
+	const contract = new ethers.Contract(contractAddress, artifact.abi, provider);
+	// const { METHOD_NAME } = contract.functions;
+
+	return (<h1>Hello, TodoList Contract.</h1>)
+}
