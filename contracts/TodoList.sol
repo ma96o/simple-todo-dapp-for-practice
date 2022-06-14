d492a2aa711fa09e760d36f6ac87e7950d0fb4cb
masaaki1915 <masaaki1915@gmail.com>
2022-06-14 15:27:39 +0900

    add contract functions to TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,33 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract TodoList {
+	event Created(uint id, string content);
+	event UpdateIsCompleted(uint id, bool completed);
 
+	struct Task {
+		string content;
+		bool isCompleted;
+	}
+
+	uint public taskCount = 0;
+	mapping (uint => Task) public tasks;
+
+	constructor() {
+		createTask("Initial Data");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(_content, false);
+		emit Created(taskCount, _content);
+	}
+
+	function toggleCompleted(uint _id) public {
+		Task memory _task = tasks[_id];
+		_task.isCompeted = !_task.isCompleted;
+		tasks[_id] = _task;
+		emit updateIsCompleted(_id, _task.isCompeted);
+	}
 }
